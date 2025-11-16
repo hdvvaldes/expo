56406f75c5a31dcf068572b7cad2f5d57e275265
hdvvaldes <hdvvaldes@gmail.com>
2025-11-16 00:02:36 -0600

    chore(coding): Setting up maven

---
+++ doubleEndedQ/src/main/java/mx/unam/ciencias/expo/Ejemplos.java
@@ -0,0 +1,16 @@
+package mx.unam.ciencias.expo;
+
+
+/**
+ * 
+ * Ejemplos para casos de uso de las double ended priority queues.
+ *
+ */
+public class Ejemplos{
+
+    public static void main(String[] args) {
+
+        System.out.println("Ejemplos de double ended priority queues");
+        
+    }
+}
