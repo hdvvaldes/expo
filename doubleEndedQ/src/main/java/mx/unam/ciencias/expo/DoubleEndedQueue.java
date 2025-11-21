f87b9a20700709666fb15e4d3087d95aabc7c0f4
hdvvaldes <hdvvaldes@gmail.com>
2025-11-21 16:11:28 -0600

    implementation(body interface): Implementation of strategy pattern usign inteface DoubleEndedQueue.BodyDoubleEndedQueue

--- doubleEndedQ/src/main/java/mx/unam/ciencias/expo/DoubleEndedQueue.java
+++ doubleEndedQ/src/main/java/mx/unam/ciencias/expo/DoubleEndedQueue.java
@@ -2,48 +2,100 @@ package mx.unam.ciencias.expo;
 
 
 
-public abstract class DoubleEndedQueue<T> {
+public class DoubleEndedQueue<T> {
+
+    public interface BodyDoubleEndedQueue<T> {
+
+        /*
+         * Inserts an element into the double ended queue.
+         * @param t the element to insert.
+         *
+         */
+        public void put(T t);
+
+        /*
+         * Removes and returns the minimum element in the double ended queue.
+         * @return the minimum element in the double ended queue.
+         *
+         */
+        public T removeMin();
+
+        /*
+         * Removes and returns the maximum element in the double ended queue.
+         * @return the maximum element in the double ended queue.
+         *
+         */
+        public T removeMax();
+        /*
+         * Returns the minimum element in the double ended queue.
+         * @return the minimum element in the double ended queue.
+         *
+         */
+        public T getMin();
+
+        /*
+         * Returns the maximum element in the double ended queue.
+         * @return the maximum element in the double ended queue.
+         *
+         */
+        public T getMax();
+
+    }
     
     /* Fields */
-    private T min;
 
-    private T max;
+    private BodyDoubleEndedQueue<T> body;
 
-    /* Methods */    
+    public DoubleEndedQueue(BodyDoubleEndedQueue<T> body) {
+        this.body = body;
+
+    }
+
+    /**********  Methods ************/    
     
     /*
      * Returns the minimum element in the double ended queue.
      * @return the minimum element in the double ended queue.
      *
      */
-    public T getMin() { return min; }
+    public T getMin() { 
+        return body.getMin(); 
+    }
 
     /*
      * Returns the maximum element in the double ended queue.
      * @return the maximum element in the double ended queue.
      *
      */
-    public T getMax() { return max; }
+    public T getMax() { 
+        return body.getMax(); 
+    }
 
     /*
      * Inserts an element into the double ended queue.
      * @param t the element to insert.
      *
      */
-    public abstract void put(T t);
+    public void put(T t){
+        body.put(t);
+    }
 
     /*
      * Removes and returns the minimum element in the double ended queue.
      * @return the minimum element in the double ended queue.
      *
      */
-    public abstract T removeMin();
+    public T removeMin() {
+        return body.removeMin();
+    }
 
     /*
      * Removes and returns the maximum element in the double ended queue.
      * @return the maximum element in the double ended queue.
      *
      */
-    public abstract T removeMax();
+    public T removeMax(){
+        return body.removeMax();
+    }
 
 }
